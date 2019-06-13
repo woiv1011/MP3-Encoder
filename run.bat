c6e5e6bf2b00450fb95904134979d380221f9e70
Ivar Wohlfeil <ivar.wohlfeil+github@gmail.com>
2019-06-13 04:40:38 +0200

    removed unnecessary files

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-REM tcc main.c -g -I"C:\Users\Ivar\Desktop\MP3-Encoder\lame-3.100\include" -vv
-REM -vv to show included files
-REM -L"C:\Users\Ivar\Desktop\MP3-Encoder\lame-3.100\include"
-REM where command = which
-REM main "C:\Users\Ivar\Desktop\MP3-Encoder"
-REM main "../wavFiles/"
-main.exe "./wavFiles/"
